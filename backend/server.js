5d0f0961228113da2c18df0dc3b27cf9b93f11c2
Jia XU <xujia118@hotmail.com>
2024-07-07 16:02:46 -0700

    Built

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 const port = 3000;
@@ -7,15 +8,16 @@ const port = 3000;
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use(express.static("../frontend/dist"));
 
 // Summary routes
 import summaryRouter from "./routes/summary.js";
-app.use("/api", summaryRouter)
+app.use("/api", summaryRouter);
 
 // Queries routes
-import queryRouter from "./routes/queries.js"
-app.use("/api", queryRouter)
+import queryRouter from "./routes/queries.js";
+app.use("/api", queryRouter);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
